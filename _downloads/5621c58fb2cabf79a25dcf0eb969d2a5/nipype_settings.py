fc7baeb5a024e66e78f01b6a306afe74ecfbff49
matthew-brett <matthew-brett@users.noreply.github.com>
2022-09-26 23:22:47 +0000

    deploy: 6c3b793167566d3a7fd34beb8967086e3d0fb6dc

---
+++ _downloads/5621c58fb2cabf79a25dcf0eb969d2a5/nipype_settings.py
@@ -0,0 +1,5 @@
+# Null settings for nipype.
+from nipype.interfaces import matlab as nim
+
+nim.MatlabCommand.set_default_matlab_cmd('/Applications/MATLAB_R2022b.app/bin/matlab')
+nim.MatlabCommand.set_default_paths('/Users/mb312/dev_trees/spm12')
